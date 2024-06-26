91ff81256604747ed502bea965b285f954e43406
Fabiano Martins de Sales <fabianomartinsdev@gmail.com>
2024-06-26 18:02:04 -0300

    Create footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,7 +1,28 @@
-import React from "react";
+import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa";
+
+import styles from "./Footer.module.css";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.footerContainer}>
+        <ul className={styles.list}>
+          <li>
+            <FaFacebook />
+          </li>
+          <li>
+            <FaInstagram />
+          </li>
+          <li>
+            <FaLinkedin />
+          </li>
+        </ul>
+        <p className={styles.copyright}>
+          <span>Costs</span> &copy; 2024
+        </p>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
