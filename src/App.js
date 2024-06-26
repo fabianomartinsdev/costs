04f273062d77427065f5dda6957944ea876c6f5f
Fabiano Martins de Sales <fabianomartinsdev@gmail.com>
2024-06-26 17:47:11 -0300

    Create navbar component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,31 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./components/pages/Home";
 import Company from "./components/pages/Company";
 import Contact from "./components/pages/Contact";
 import NewProject from "./components/pages/NewProject";
 import Container from "./components/layout/Container";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
 
 function App() {
   return (
     <BrowserRouter>
-      <ul>
-        <Link to="/">Home</Link>
-
-        <Link to="/Company">Empresa</Link>
-
-        <Link to="/Contact">Contato</Link>
-
-        <Link to="/NewProject">Novo Projeto</Link>
-      </ul>
+      <Navbar />
 
       <Container customClass="min-height">
         <Routes>
           <Route path="/" element={<Home />} />
 
-          <Route path="/Company" element={<Company />} />
+          <Route path="/company" element={<Company />} />
 
-          <Route path="/Contact" element={<Contact />} />
+          <Route path="/contact" element={<Contact />} />
 
-          <Route path="/NewProject" element={<NewProject />} />
+          <Route path="/newproject" element={<NewProject />} />
         </Routes>
       </Container>
 
-      <p>Footer</p>
+      <Footer />
     </BrowserRouter>
   );
 }
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,10 +1,40 @@
-import React from "react";
+import { Link } from "react-router-dom";
+
+import styles from "./Navbar.module.css";
+import logo from "../../images/costs_logo.png";
 
 function Navbar() {
   return (
-    <section>
-      <h1>Costs</h1>
-    </section>
+    <header className={styles.header}>
+      <nav className={styles.navbar}>
+        <Link to="/">
+          <img src={logo} alt="" width="35px" />
+        </Link>
+        <ul className={styles.list}>
+          <li>
+            <Link to="/" className={styles.link}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/newproject" className={styles.link}>
+              Novo Projeto
+            </Link>
+          </li>
+          <li>
+            <Link to="/company" className={styles.link}>
+              Empresa
+            </Link>
+          </li>
+
+          <li>
+            <Link to="/contact" className={styles.link}>
+              Contato
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
 
