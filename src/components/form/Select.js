6c8c36adac355fb8274b43d60c31d8a200653e59
Fabiano Martins de Sales <fabianomartinsdev@gmail.com>
2024-06-27 14:34:58 -0300

    Create fake API/db

--- src/components/form/Select.js
+++ src/components/form/Select.js
@@ -6,6 +6,11 @@ function Select({ text, name, options, handleOnChange, value }) {
       <label htmlFor="">{text}:</label>
       <select name={name} id={name} value={value} onChange={handleOnChange}>
         <option>Selecione uma opção</option>
+        {options.map((option) => (
+          <option key={option.id} value={option.id}>
+            {option.name}
+          </option>
+        ))}
       </select>
     </div>
   );
